2127e2dbebded79ced224d71ffb8e098fddb5f6c
sigmacodeslol <m96153045@gmail.com>
2025-09-13 13:47:07 +0100

    chore: add first_nonempty

--- lua/def/f.lua
+++ lua/def/f.lua
@@ -7,4 +7,17 @@ function M.fn(f, ...)
   end
 end
 
+---Return the first non-empty value for a given key in a table
+---@param tbl table[]
+---@param key string
+---@param fallback any
+---@return any
+function M.first_nonempty(tbl, key, fallback)
+  for _, v in ipairs(tbl or {}) do
+    if v[key] and v[key] ~= "" then
+      return v[key]
+    end
+  end
+  return fallback
+end
 return M
